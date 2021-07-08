2b1c1d323f4243991007524a01e33325c183d33e
min <wyes91622@gmail.com>
2021-07-09 01:39:17 +0800

    Add isAdmin field

--- models/userModel.js
+++ models/userModel.js
@@ -1,29 +1,33 @@
 let mongoose = require("mongoose");
 
 let userSchema = new mongoose.Schema({
-	sid: {
-		type: String,
-		match: /^[A-Z][0-9]{8}$/,
-		required: true,
-		unique: true,
-		uppercase:true
-	},
-	username: {
-		type: String,
-		require: true,
-	},
-	password: {
-		type: String,
-		validate: (v) => {
-			return v.length === 64;
-		},
-		require: true,
-	},
-	shiftTime: {
-		type: [Number],
-		enum: [1, 2, 3, 4, 5, 6, 7],
-		default: [],
-	},
+  sid: {
+    type: String,
+    match: /^[A-Z][0-9]{8}$/,
+    required: true,
+    unique: true,
+    uppercase: true,
+  },
+  username: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    validate: (v) => {
+      return v.length === 64;
+    },
+    require: true,
+  },
+  shiftTime: {
+    type: [Number],
+    enum: [1, 2, 3, 4, 5, 6, 7],
+    default: [],
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("userModel", userSchema);
