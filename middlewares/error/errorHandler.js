d0697f2e9d4c3b1b666cae85469ca4ec82b05957
min <wyes91622@gmail.com>
2021-07-11 17:29:40 +0800

    Unified use of appError

--- middlewares/error/errorHandler.js
+++ middlewares/error/errorHandler.js
@@ -3,18 +3,22 @@ const { ValidationError } = require("express-validation");
 const { UnauthorizedError } = require("express-jwt");
 
 function errorHandler(err, req, res, next) {
+  let resError;
+
   if (err instanceof ValidationError) {
-    return res.status(400).json({ message: "INVALID_FORMDATA" });
-  } else if (err instanceof appError) {
-    // 從enum中找出errorMessage
-    let errorMessage = Object.keys(appError.errorMessageEnum).find((key) => {
-      return appError.errorMessageEnum[key] === err.errorMessage;
-    });
-    return res.status(err.stateCode).json({ message: errorMessage });
-  } else if (err instanceof UnauthorizedError) { //JWT 錯誤
-    return res.status(401).json({ message: "INVALID_TOKEN" });
+    resError = new appError(appError.errorMessageEnum.INVALID_FORMDATA, 400);
+  } else if (err instanceof UnauthorizedError) {
+    //JWT 錯誤
+    resError = new appError(appError.errorMessageEnum.INVALID_TOKEN, 401);
+  } else {
+    resError = err;
   }
-  return res.status(500).json(err);
+
+  // 從enum中找出errorMessage
+  let errorMessage = Object.keys(appError.errorMessageEnum).find((key) => {
+    return appError.errorMessageEnum[key] === resError.errorMessage;
+  });
+  return res.status(resError.stateCode).json({ message: errorMessage });
 }
 
 module.exports = errorHandler;
